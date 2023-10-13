217c56a3577f35a22e737df048f8f1eda7141959
Alexey Kostin <a.kostin@rogii.com>
2023-10-13 15:51:24 +0400

    Add a check for Q_EMIT usage

--- README.md
+++ README.md
@@ -7,9 +7,10 @@ Add this to your `.pre-commit-config.yaml`
 
 ```yaml
 -   repo: https://github.com/rogii-com/pre-commit-hooks
-    rev: v1.0.0  # Use the ref you want to point at
+    rev: v1.1.0  # Use the ref you want to point at
     hooks:
     -   id: check-line-endings
     -   id: check-trailing-whitespaces
     -   id: check-end-of-file
+    -   id: check-q-emit
 ```
---
+++ pre_commit_hooks/check_q_emit.py
@@ -0,0 +1,36 @@
+import argparse
+import re
+from typing import Optional
+from typing import Sequence
+
+
+def check_q_emit_in_file(filename: str) -> bool:
+    result = True
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+        for i in range(len(lines)):
+            line = lines[i]
+            if re.search('Q_EMIT\\s*\(', line):
+                print(f'{filename}:{i + 1}: Incorrect Q_EMIT usage (the macro should not be used as callable):\n{line}', end='')
+                result = False
+
+    return result
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*', help='Filenames to check')
+    args = parser.parse_args(argv)
+
+    return_code = 0
+    for filename in args.filenames:
+        if not filename.endswith(('.cpp', '.hpp')):
+            continue
+        if not check_q_emit_in_file(filename):
+            return_code = 1
+    return return_code
+
+
+if __name__ == '__main__':
+    exit(main())
